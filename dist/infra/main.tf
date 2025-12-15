c420443aff0004ef231f69a5616dd94a44db7be4
prachiti <paraskarprachiti@gmail.com>
2025-12-15 06:12:19 -0500

    Add complete Terraform infrastructure code

---
+++ dist/infra/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "app-security-group"
+  description = "Allow HTTP and SSH inbound traffic"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  tags = {
+    Name = "Blog-App-Server"
+  }
+}
+
+resource "aws_ecr_repository" "app_repo" {
+  name = "clean-blog-app"
+  tags = {
+    Name = "Blog-App-Repo"
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ecr-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read_only" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-ecr-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
